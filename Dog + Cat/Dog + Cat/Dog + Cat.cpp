30fac09f62b7967d3fe0132b1d2e8985ff4ac860
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2024-02-02 17:22:11 +0100

    Add files via upload

---
+++ Dog + Cat/Dog + Cat/Dog + Cat.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+class Dog; 
+
+class Cat {
+public:
+    string name;
+
+    Cat()
+    {
+    }
+
+    Cat(const Dog& dog); 
+
+    void Meow() {
+        cout << "MEOW\n";
+    }
+};
+
+class Dog {
+public:
+    string name;
+
+    Dog()
+    {
+    }
+
+    Dog(const Cat& cat);
+   
+    void Bark() {
+        cout << "BARK\n";
+    }
+};
+
+Cat::Cat(const Dog& dog) {
+    name = dog.name;
+}
+
+Dog::Dog(const Cat& cat) {
+    name = cat.name;
+}
+
+int main() 
+{
+    Cat c;
+    c.name = "Barsik";
+    cout << c.name << "\n";
+    c.Meow();
+    cout << "\n\n\n";
+    Dog d = c; 
+    d.Bark();
+    cout << d.name << "\n";
+    //Cat x = d;
+    //x.Meow();
+    //cout << x.name << "\n"; 
+}
